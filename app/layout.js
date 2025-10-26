f7f4507cc6dc17e08a11068c748a19577e797ea4
josealgara-design <jose.algara@gmail.com>
2025-10-26 09:34:35 -0700

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,15 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'Jose Algara - Supply Chain & Product Leadership',
+  description: 'VP Supply Chain leader with 15+ years building transformative technology solutions',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
+```
