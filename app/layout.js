3038604bf950650531bbbcf2d71daa09b48ee432
josealgara-design <jose.algara@gmail.com>
2025-11-02 18:28:23 -0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
 
 export const metadata = {
-  title: 'Jose Algara - Supply Chain & Product Leadership',
+  title: 'Jose Algara - Supply Chain Transformation | Executive Coaching',
   description: 'VP Supply Chain leader with 15+ years building transformative technology solutions',
 }
 
